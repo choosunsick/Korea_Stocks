64365d4af73084119fbe7cc10775a4e9c24bf28a
choosunsick <random@example.com>
2019-04-23 20:21:36 +0900

    2019 04 23 update
    code update complete

--- CodeAndTool/Stock_crawling.py
+++ CodeAndTool/Stock_crawling.py
@@ -1,150 +1,105 @@
 #!/usr/local/opt/python/bin/python3.7
 
-import asyncio
+import asyncio 
 import aiohttp
-from aiohttp import ClientSession
 import json
-import pandas as pd
 import os
+import pandas as pd
 from datetime import datetime
 
-total_code = pd.read_csv("total_stocklist.csv")
+total_temp = pd.read_csv("total_stocklist.csv")
 
-today = datetime.today().strftime("%Y %m %d")
-today_normal_list = total_code[total_code[str.replace(today," ","_")]=="normal"]
-
-normal_list_fullcode =[]
+today_date = str.replace(datetime.today().strftime("%Y %m %d")," ","_")
 
 normal_list_fullcode =[]
-for i in today_normal_list.code:
+for i in total_temp['code']:
     normal_list_fullcode.append(str(i).zfill(6))
 
-pd.options.mode.chained_assignment = None
+test_json={}
+for i in range(0,len(total_temp)):
+    if total_temp['구분'][i]=='Kospi':
+        KS_KQ="KS"
+    else:
+        KS_KQ="KQ"
+    test_json.update({normal_list_fullcode[i]:{'name':total_temp.name[i],'is_KS':KS_KQ,'Date':{today_date:total_temp[today_date][i]}}})
 
-today_normal_list.code = normal_list_fullcode
+test12 = [{'code':i,'KS&KQ':j['is_KS']} for i,j in test_json.items() if j['Date'][today_date] == 'normal']
 
-kospi_normal_list=today_normal_list[today_normal_list.구분=="Kospi"]
-kosdaq_normal_list=today_normal_list[today_normal_list.구분=="Kosdaq"]
+temp_25 = []
 
-ks_urls = []
-for x in kospi_normal_list.code:
-    url = 'https://finance.yahoo.com/quote/'+str(x)+'.KS/history?p='+ str(x) + '.KS'
-    ks_urls.append(url)
+for i in range(0,int(len(test12)/25)):
+    x=list(range(0,len(test12)+1,25))
+    temp_25.append(test12[x[i]:x[i+1]])    
 
-kq_urls = []
-for x in kosdaq_normal_list.code:
-    url = 'https://finance.yahoo.com/quote/'+str(x)+'.KQ/history?p='+ str(x) + '.KQ'
-    kq_urls.append(url)
+temp_25.append(test12[-(len(test12)-(int(len(test12)/25)*25)):])  
 
-full_urls = ks_urls+kq_urls
+def parse_html(text):
+    if len(text.split('\"HistoricalPriceStore\":'))==2:
+        stock_number = text.split("symbol")[0].split("title")[1].split(" (")[1].split(") ")[0].split(".")[0]
+        stock_number_str = f'{stock_number}.csv'
+        temp1 = text.split('\"HistoricalPriceStore\":')[1].split('],"isPending":false,"')[0] 
+        temp1 = json.dumps(temp1)
+        temp1 = temp1.replace('\\','')[11:]
+        yahoo_json = json.loads((temp1[:len(temp1)-1]+']'))
+        return{"json":yahoo_json,"stock_number":stock_number_str}
 
-urls_25 = []
-for i in range(0,int(len(full_urls)/25)):
-    x=list(range(0,len(full_urls)+1,25))
-    urls_25.append(full_urls[x[i]:x[i+1]])
+def write_csv(yahoo_json):
+    test = json.dumps(yahoo_json['json'])
+    test = pd.read_json(test, orient='records')
+    test2 = pd.concat([test['open'][:30], test['high'][:30],test['low'][:30],test['close'][:30],test['volume'][:30],test['adjclose'][:30]], axis=1).fillna(0).astype(int)
+    test2 = test2.set_index(test['date'][:30])
+    test2.index = test2.index[:].strftime("%Y-%m-%d")
+    test2 = test2[['open','high','low','close','volume','adjclose']]
+    test2.columns = ['Open','High','Low','Close','Volume','Adj_Close']
+    savename = f"../temp/{yahoo_json['stock_number']}"
+    test2.to_csv(savename,index_label='Date')
 
-urls_25.append(full_urls[-(len(full_urls)-(int(len(full_urls)/25)*25)):])    
-    
-html_dict = {}
-not_error_urls = []
-error_urls = []
-new_yahoo_error=[]
+error_list_404 = []
 
-async def fetch(session,url):
-    bounde_sempahore = asyncio.BoundedSemaphore(200)
-    async with bounde_sempahore:
-        async with await session.get(url,timeout=30,allow_redirects=True) as response:
-            print(response.status)
-            if response.status == 404:
-                await asyncio.sleep(60)
-            return await response.text()
+def stock_download(code_list):
 
-new_yahoo_error=[]
-def html_clean(html_dict):
-    for i in range(0,len(html_dict)):
-        print(list(html_dict.keys())[i])
-        html = list(html_dict.values())[i]
-        temp1 = html.split('\"HistoricalPriceStore\":')[1] 
-        temp2 = temp1.split('],"isPending":false,"')[0]
-        temp3 = json.dumps(temp2)
-        temp4 = temp3.replace('\\','')[11:]
-        yahoo_json = json.loads((temp4[:len(temp4)-1]+']'))
-        test = json.dumps(yahoo_json)
-        test = pd.read_json(test, orient='records')
-        if len(test)==0:
-            print("nodata on yahoo")
-            new_yahoo_error.append(list(html_dict.keys())[i])
-            pass
-        else:
-            test2 = pd.concat([test['open'][:30], test['high'][:30],test['low'][:30],test['close'][:30],test['volume'][:30],test['adjclose'][:30]], axis=1).fillna(0).astype(int)
-            test2 = test2.set_index(test['date'][:30])
-            test2.index = test2.index[:].strftime("%Y-%m-%d")
-            test2.columns = ['Open','High','Low','Close','Volume','Adj_Close']
-            savename = "../temp/"+list(html_dict.keys())[i]+".csv"
-            test2.to_csv(savename,index_label='Date')
+    results = []
 
-async def get_site_content(url):
-    conn = aiohttp.TCPConnector(limit=None)
-    async with aiohttp.ClientSession(connector=conn) as session:
+    async def download(code: str, KSorKQ:str):
+        bounde_sempahore = asyncio.BoundedSemaphore(200)
+        conn = aiohttp.TCPConnector(limit=None)
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with bounde_sempahore:
+                async with session.get(f'https://finance.yahoo.com/quote/{code}.{KSorKQ}/history?p={code}.{KSorKQ}',allow_redirects=True,timeout=30) as resp:
+                    print(resp.status)
+                    try:
+                        results.append({'code': code, 'text': await resp.text(), "KS&KQ":KSorKQ})
+                    except:
+                        error_list_404.append({'code':code,"KS&KQ":KSorKQ})
+
+    tasks = [download(item['code'], item['KS&KQ']) for item in code_list]
+
+    asyncio.run(asyncio.wait(tasks))
+    return{'result':results,'error_list':error_list_404}
+
+error_list = []
+
+def full_stack(code_list):
+    temp = stock_download(code_list)
+    for item in temp['result']:
         try:
-            text = await fetch(session, url)
-        except:    
-            print("connection_fail")
-        else:
-            if len(text.split('\"HistoricalPriceStore\":'))==2:
-                not_error_urls.append(url)
-                #print(url)
-                return html_dict.update({url[-9:-3]:text})
-            else:
-                error_urls.append(url) 
-    
-async def task(urls):
-    return await asyncio.wait([get_site_content(i) for i in urls])
-
-def cover(urls):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(task(urls))
-    loop.close()
-
-[cover(url) for url in urls_25]
-
-html_clean(html_dict)
-
-
-html_dict = {}
-
-server_error_urls = [x for x in full_urls if x not in not_error_urls]
+            write_csv(parse_html(item['text']))
+        except:
+            error_list.append({'code':item['code'],"KS&KQ":item['KS&KQ']})
         
+    return error_list
 
-if len(server_error_urls)!=0:
-    cover(server_error_urls)
-    html_clean(html_dict)
+final_error_list = []
+def retry_error_list(error_list):
+    if len(error_list)!=0:
+        temp = stock_download(error_list)
+        for item in temp['result']:
+            try:
+                write_csv(parse_html(item['text']))
+            except:
+                final_error_list.append(error_list)
 
-final_error_urls = []
-if len(not_error_urls)!=len(full_urls):
-    for x in full_urls:
-        if x not in not_error_urls:
-            final_error_urls.append(x)
-else:
-    final_error_urls = []
-    print("no remain urls")
-
-
-date = datetime.today().strftime("%m %d")
-date = str.replace(date," ","_")
-
-error_list = pd.DataFrame({"urls":final_error_urls})
-error_list.to_csv("./"+"error_list_" + date + ".csv",index=False)
-
-crawling_list = pd.DataFrame({"urls":not_error_urls})
-crawling_list.to_csv("./"+"crawling_list_"+  date + ".csv",index=True)
-
-if len(new_yahoo_error)==0:
-    print("no new error")
-else:
-    temp_yahoo_error_list =  pd.DataFrame({"urls":new_yahoo_error})
-    temp_yahoo_error_list.to_csv("./"+"new_yahoo_error_list"+  date + ".csv",index=True)
+    return final_error_list
 
 
 def mergeStock(stockNumber):
@@ -204,9 +159,31 @@ def merge_about_2000(stockNumber):
         savename="../Korea_Stocks_Full/"+stockNumber
         stockData.to_csv(savename,index_label='Date')
 
+for i in temp_25:
+    full_stack(i)
+
+print(len(error_list))
+print(len(error_list_404))
+
+retry_error_list(error_list)
+retry_error_list(error_list_404)
+
+date = str.replace(datetime.today().strftime("%Y %m %d")," ","-")
 
 list1=os.listdir("../temp/")
 list1=[s for s in list1 if "csv" in s]
+re_list = []
 for i in list1:
     mergeStock(i)
     merge_about_2000(i)
+    temp = pd.read_csv(f"../temp/{i}",index_col=0, parse_dates=True, dayfirst=True)
+    if date not in temp.index:
+        re_list.append(i)
+
+date_false = pd.DataFrame({"urls":re_list})
+date_false.to_csv("./"+"date_error_" + today_date + ".csv",index=False)
+
+error_list = pd.DataFrame({"code":final_error_list})
+error_list.to_csv("./"+"error_list_" + date + ".csv",index=False)
+
+
