0b2da7bcac2df488952121515a3aa619756d4f6c
choosunsick <random@example.com>
2019-11-21 17:39:41 +0900

    2019 11 21 update

--- CodeAndTool/Stock_crawling.py
+++ CodeAndTool/Stock_crawling.py
@@ -23,13 +23,13 @@ for i in range(0,len(total_temp)):
 
 test12 = [{'code':i,'KS&KQ':j['is_KS']} for i,j in test_json.items() if j['Date'][today_date] == 'normal']
 
-temp_10 = []
+temp_15 = []
 
-for i in range(0,int(len(test12)/10)):
-    x=list(range(0,len(test12)+1,10))
-    temp_10.append(test12[x[i]:x[i+1]])    
+for i in range(0,int(len(test12)/15)):
+    x=list(range(0,len(test12)+1,15))
+    temp_15.append(test12[x[i]:x[i+1]])    
 
-temp_10.append(test12[-(len(test12)-(int(len(test12)/10)*10)):])  
+temp_15.append(test12[-(len(test12)-(int(len(test12)/15)*15)):])  
 
 def parse_html(text):
     try:
@@ -162,7 +162,7 @@ def merge_about_2000(stockNumber):
         savename="../Korea_Stocks_Full/"+stockNumber
         stockData.to_csv(savename,index_label='Date')
 
-for i in temp_10:
+for i in temp_15:
     full_stack(i)
 
 print(len(error_list))
